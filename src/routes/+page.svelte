8405aeeeae3f3f2f6828ad4c84f5e0cc5f512c0b
SomeGuy <51111827+S3bTheGuy@users.noreply.github.com>
2023-04-15 18:32:10 +0200

    Tons of optimization, and site now renders well on all device types

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -190,7 +190,7 @@
         color: rgb(158, 123, 218);
     }
 
-    @media (max-width: 600px) or (max-height: 600px) {
+    @media (max-width: 700px) or (max-height: 700px) {
         .title {
             font-size: 1.5rem;
         }
